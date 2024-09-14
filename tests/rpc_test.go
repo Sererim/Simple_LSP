c7e7bd6d4f35eb41873b8800d24394a501dd4e9a
Sererim <danielleymanw@gmail.com>
2024-09-14 15:42:34 +0300

    Initial commit.

--- tests/rpc_test.go
+++ tests/rpc_test.go
@@ -16,3 +16,15 @@ func TestEncode(t *testing.T) {
 		t.Fatalf("\nExpected: %s\nActual: %s", expected, actual)
 	}
 }
+
+func TestDecode(t *testing.T) {
+	incoming_msg := "Content-Length: 16\r\n\r\n{\"Testing\"true}"
+	content_length, err := rpc.DecodeMessage([]byte(incoming_msg))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if content_length != 16 {
+		t.Fatalf("Expected: 16\nActual: %s", content_length)
+	}
+}
